20631553eaad90ae94d5ff070069d2b1f7ec4b2a
Soraia <sorilc@hotmail.com>
2024-04-25 19:49:05 +0200

    tab template

--- twitter-clone/components/SideBar/LeftBar.vue
+++ twitter-clone/components/SideBar/LeftBar.vue
@@ -1,17 +1,28 @@
 <template>
-    <div class="bg-red-100 h-screen flex flex-col">
+    <div class="h-screen flex flex-col">
         <div class="w-min p-2 my-2 rounded-full hover:bg-blue-50
-        dark:hover:bg-white/20" :class="transition"
+        dark:hover:bg-white/20" :class="defaultTransition"
         >
             <nuxt-link to="/">
                 <div class="w-8 h-8">
                     <IconsTwitter/>
-                    <h1>adw</h1>
                 </div>
             </nuxt-link>   
         </div>
+
+    <SideBarTab>
+        <template v-slot:icon>
+            <IconsHomeIcon/>
+        </template>
+        <template v-slot:name>
+            Home
+        </template>
+    </SideBarTab>
+
     </div>
 </template>
 <script setup>
-    const transition = 'transition ease-in-out duration-350'
+import useTailwindConfig from '~/composbles/useTailwindConfig';
+
+const { defaultTransition } = useTailwindConfig
 </script>
---
+++ twitter-clone/components/SideBar/Tab.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <nux-link to="#" class="flex items-center p-3 w-min hover:bg-gray-200 rounded-full" :class="defaultTransition">
+            <div class="w-6 h-6 text-dark" :class="defaultTransition">
+                <slot name="icon"></slot>
+            </div>
+
+            <div class="ml-4 text-lg">
+                <slot name="name"></slot>
+            </div>
+        </nux-link>
+    </div>
+</template>
+
+<script setup>
+import useTailwindConfig from '~/composbles/useTailwindConfig';
+
+const { defaultTransition } = useTailwindConfig
+
+const props = defineProps({
+    active: {
+        type: Boolean,
+        default: false
+    }
+})
+
+
+</script>

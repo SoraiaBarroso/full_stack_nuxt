e931616690559e3be2c33dd615b868182379b6c3
Soraia <sorilc@hotmail.com>
2024-05-04 19:36:05 +0200

    fixing path tweet image

--- twitter-clone/server/api/user/tweets/index.post.js
+++ twitter-clone/server/api/user/tweets/index.post.js
@@ -1,10 +1,13 @@
 import formidable from "formidable"
 import { createTweet } from "~/server/db/tweets"
 import { tweetTransformer } from "~/server/transformers/tweet"
+import { createMediaFile } from "~/server/db/mediaFiles"
+import { uploadToCloudinary } from "../../../utils/cloudinary.js"
 
 export default defineEventHandler(async (event) => {
+
     const form = formidable({})
-    
+
     const response = await new Promise((resolve, reject) => {
         form.parse(event.node.req, (err, fields, files) => {
             if (err) {
@@ -17,15 +20,39 @@ export default defineEventHandler(async (event) => {
     const { fields, files } = response
 
     const userId = event.context?.auth?.user?.id
-    
+
     const tweetData = {
         text: fields.text[0],
         authorId: userId
     }
-   
-    // const tweet = await createTweet(tweetData)
+
+    const replyTo = fields.replyTo
+
+    if (replyTo && replyTo !== 'null' && replyTo !== 'undefined') {
+        tweetData.replyToId = replyTo
+    }
+
+    const tweet = await createTweet(tweetData)
+
+    const filePromises = Object.keys(files).map(async key => {
+        const file = files[key]
+
+        // console.log("files: ", file[0].filepath)
+
+        const cloudinaryResource = await uploadToCloudinary(file[0].filepath)
+
+        return createMediaFile({
+            url: cloudinaryResource.secure_url,
+            providerPublicId: cloudinaryResource.public_id,
+            userId: userId,
+            tweetId: tweet.id
+        })
+    })
+
+    await Promise.all(filePromises)
 
     return {
         // tweet: tweetTransformer(tweet)
+        files
     }
 })

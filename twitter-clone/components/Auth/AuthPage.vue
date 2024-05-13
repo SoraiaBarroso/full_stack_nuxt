c22919c3ab6fec3ffb16683205951c3840a4fcbe
Soraia <sorilc@hotmail.com>
2024-05-13 12:31:29 +0200

    latest

--- twitter-clone/components/Auth/AuthPage.vue
+++ twitter-clone/components/Auth/AuthPage.vue
@@ -4,12 +4,12 @@
             <img src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80" class="absolute inset-0 object-cover w-full h-full" alt="Auth Image">
         </div>
     
-        <div class="flex flex-col justify-center flex-1 px-4 py-12 sm:px-6
+        <div class="flex flex-col justify-center items-center flex-1 px-4 py-12 sm:px-6
         lg:flex-none lg:px-20 xl:px-24">
                 <AuthFormLogIn v-if="isLoginForm"/> 
                 <AuthFormRegister v-else />             
-                <span v-if="isLoginForm" class="dark:text-white pt-5 ml-1 cursor-default text-gray-700 text-sm">Don't have an account?<button class="text-blue-600 ml-1 dark:text-blue-300" @click="switchForm">Register</button></span>
-                <span v-else class="dark:text-white pt-5 ml-1 cursor-default text-gray-700 text-sm">Already an user?<button class="text-blue-600 ml-1 dark:text-blue-300" @click="switchForm">Log In</button></span>
+                <span v-if="isLoginForm" class="dark:text-white w-80 pt-5 ml-1 cursor-default text-gray-700 text-sm">Don't have an account?<button class="text-blue-600 ml-1 dark:text-blue-300" @click="switchForm">Register</button></span>
+                <span v-else class="dark:text-white w-80 pt-5 ml-1 cursor-default text-gray-700 text-sm">Already an user?<button class="text-blue-600 ml-1 dark:text-blue-300" @click="switchForm">Log In</button></span>
         </div>
     </div>
 </template>

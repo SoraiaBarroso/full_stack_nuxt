f75fde1ee270655cb24f7c3629a883c3e9a93d47
Soraia <sorilc@hotmail.com>
2024-05-14 19:18:48 +0200

    more validation to log-in

--- twitter-clone/app.vue
+++ twitter-clone/app.vue
@@ -1,51 +1,53 @@
 <template>
-  <n-message-provider>
-    <div :class="{'dark': darkMode}">
-      <div class="bg-white dark:bg-dim-900">
+  <n-config-provider :theme-overrides="themeOverrides">
+    <n-message-provider>
+      <div :class="{'dark': darkMode}">
+        <div class="bg-white dark:bg-dim-900">
 
-        <!-- Loading page  -->
-        <LoadinPage v-if="isAuthLoading"/>
+          <!-- Loading page  -->
+          <LoadinPage v-if="isAuthLoading"/>
 
-        <!-- App  -->
-        <div v-else-if="user" class="min-h-full">
-          <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-6 lg:gap-5">
-              
-                <!-- left -->
-                <div class="md:block md:col-span-1 xs-col-span-1 xl:col-span-2 overflow-y-auto">
-                  <div class="sticky top-0">
-                    <SideBarLeftBar/>
+          <!-- App  -->
+          <div v-else-if="user" class="min-h-full">
+            <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-6 lg:gap-5">
+                
+                  <!-- left -->
+                  <div class="md:block md:col-span-1 xs-col-span-1 xl:col-span-2 overflow-y-auto">
+                    <div class="sticky top-0">
+                      <SideBarLeftBar/>
+                    </div>
                   </div>
-                </div>
 
-                <!-- Main -->
-                <main class="col-span-11 md:col-span-8 xl:col-span-6 overflow-y-auto">
-                    <NuxtPage/>
-                </main>
+                  <!-- Main -->
+                  <main class="col-span-11 md:col-span-8 xl:col-span-6 overflow-y-auto">
+                      <NuxtPage/>
+                  </main>
 
-                <!-- Right -->
-                <div class="hidden md:block xl:col-span-4 md:col-span-3 overflow-y-auto">
-                  <div class="sticky top-0">
-                    <SideBarRightBar/>
+                  <!-- Right -->
+                  <div class="hidden md:block xl:col-span-4 md:col-span-3 overflow-y-auto">
+                    <div class="sticky top-0">
+                      <SideBarRightBar/>
+                    </div>
                   </div>
-                </div>
 
-                <SpeedInsights />
+                  <SpeedInsights />
+            </div>
           </div>
+
+          <!-- Auth  -->
+          <AuthPage v-else/>
+
         </div>
-
-        <!-- Auth  -->
-        <AuthPage v-else/>
-
       </div>
-    </div>
-  </n-message-provider>
+    </n-message-provider>
+  </n-config-provider>
 </template>
 
 <script setup>
   import { SpeedInsights } from "@vercel/speed-insights/vue"
   import useAuth from "./composbles/useAuth";  
-  import { NMessageProvider } from 'naive-ui'
-
+  import { NMessageProvider, NConfigProvider  } from 'naive-ui'
+  
   const darkMode = ref(false)
 
   const { useAuthUser, initAuth, useAuthLoading } = useAuth()
@@ -55,4 +57,12 @@
   onBeforeMount(() => {
     initAuth()
   })
+
+  const themeOverrides = {
+    Input: {
+      borderHover: '1px solid #3B82F6',
+      borderFocus: '1px solid #3B82F6',
+      caretColor: '#3B82F6'
+    },
+  }
 </script>
--- twitter-clone/components/Auth/FormLogIn.vue
+++ twitter-clone/components/Auth/FormLogIn.vue
@@ -17,19 +17,7 @@
                     <NInput v-model:value="model.password" placeholder="**********"  type="password" round/>
                 </NFormItem>
             </NForm>
-           
-
-            <!-- <UIInput :invalid="data.errorMessage !== ''" placeholder="@username" label="Username *" v-model="data.username"/>
-            
-            <div v-if="data.loading" class="bg-white top-0 h-96 w-full absolute flex justify-center dark:bg-dim-900 items-center">
-                <IconsSpinner class="mb-10"/>
-            </div>
-
-            <UIInput :invalid="data.errorMessage !== ''" placeholder="*********" label="Password *" 
-            type="password" v-model="data.password"/>
-
-            <div v-if="data.errorMessage" class="text-red-500">{{ data.errorMessage }}</div> -->
-
+        
             <div>
                 <UIButton @click="handleLogin" liquid size="sm" :disabled="!isFormAvailable">Log In</UIButton>
             </div>
@@ -43,13 +31,14 @@ import useAuth from '~/composbles/useAuth';
 import { NInput, NForm, NFormItem, useMessage } from 'naive-ui'
 import { watchImmediate } from '@vueuse/core'
 
+const { login } = useAuth()
+
 const formRef = ref(null) // keep track of input values
 const message = useMessage()
 
 const model = reactive({
     username: '',
     password: '', 
-    loading: false,
     errorMessage: ''
 })
 
@@ -62,16 +51,6 @@ const rules = {
   ]
 }
 
-const { login } = useAuth()
-
-// get password and username from child component
-// const data = reactive({
-//     password: '',
-//     username: '',
-//     errorMessage: '',
-//     loading: false
-// })
-
 const isFormAvailable = ref(false)
 
 watchImmediate(model, () => {
@@ -79,16 +58,20 @@ watchImmediate(model, () => {
 }, { deep: true })
 
 async function handleLogin() {
-    model.loading = true
     try {
+        message.loading('Logging in...')
+        
         await login({
             username: model.username,
             password: model.password
         })
+        
+        message.success('Login successful!')
     } catch(error) {
-        model.errorMessage = error.message ? error.message.split('0 ')[1] : 'An error occurred' // Extract relevant part of error message        console.log(error)
+        model.errorMessage = error.message ? error.message.split('0 ')[1] : 'An error occurred' 
+        message.error(model.errorMessage)
     } finally {
-        model.loading = false
+        message.clear()
     }
 }
 
